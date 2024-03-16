ee4d0d6e847c28dd3fb14832c90cfeb664854327
Ian Scott-Cavitt <icavitt18@gmail.com>
2024-03-16 16:46:20 -0400

    in prog, combing multiplke speakers

--- combineSpeakerTexts.py
+++ combineSpeakerTexts.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import os
 import time
+import copy
 
 SUPPORTED_FILE_EXTENSIONS = (".txt")
 
@@ -9,6 +10,15 @@ class TranscribedLine:
     timeStampStart: float
     timeStampEnd: float
     lineText: str
+
+@dataclass
+class TranscibedLineWithSpeaker(TranscribedLine):
+    speaker: str
+
+@dataclass
+class SpeakerAndLines:
+    speaker: str
+    lines: list[TranscribedLine]
 
 def readFile(pathToFile) -> list[TranscribedLine]:
     arrLinesAndTimes =[]
@@ -25,8 +35,10 @@ def extractLineComponents(line) -> TranscribedLine:
     lineText = timeStampAndLineText[1].strip()
     return TranscribedLine(float(startTime), float(endTime), lineText)
 
-def removeHallucination(transcribedFile: list[TranscribedLine]) -> list[TranscribedLine]:
-    return removeRepeatedSequences(transcribedFile)
+def processWhisperTranscribedAudio(transcribedFile: list[TranscribedLine]) -> list[TranscribedLine]:
+    dehallucinatedFile = removeRepeatedSequences(transcribedFile)
+    return dehallucinatedFile
+    # return combineContiguousAudio(dehallucinatedFile)
 
 def removeRepeatHallucinations(transcribedFile: list[TranscribedLine]) -> list[TranscribedLine]:
     outerIndex = 0
@@ -37,6 +49,20 @@ def removeRepeatHallucinations(transcribedFile: list[TranscribedLine]) -> list[T
             cloneList.remove(cloneList[innerIndex])
         outerIndex = outerIndex + 1
     return cloneList
+
+# Playing with how it reads to have multiple lines of audio transcribed together
+# def combineContiguousAudio(transcribedFile: list[TranscribedLine]) -> list[TranscribedLine]:
+#     i = 0
+#     cloneList = copy.deepcopy(transcribedFile)
+#     while i < len(cloneList)-1:
+#         currLine = cloneList[i]
+#         nextLine = cloneList[i+1]
+#         while currLine.timeStampEnd == nextLine.timeStampStart:
+#             currLine.lineText = f'{currLine.lineText} {nextLine.lineText}'
+#             currLine.timeStampEnd = nextLine.timeStampEnd
+#             del cloneList[i+1:i+2]
+#         i = i +1
+#     return cloneList
 
 # This function removes repated sequences of tnrascribed lines
 # This is another type of hallucination that occurs and causes repeat junk text
@@ -65,14 +91,42 @@ def sequencesAreSame(baseSequence: list[TranscribedLine], comparedToSequnece:lis
         if(baseLine.lineText != comparedToline.lineText): return False
     return True
 
-    
+#won't support 10+ channels
+def getSpeaker(fileName: str) -> str:
+    endIndex = fileName.find('.flac')
+    return fileName[2:endIndex]
+
+def preprocessFiles(path: str, files: list[str]) -> dict[str, list[TranscribedLine]]:
+    allFiles = {}
+    for file in files:
+        if(file.endswith(SUPPORTED_FILE_EXTENSIONS)):
+            transcribedFile = readFile(f'{path}{os.sep}{file}')
+            cleanedUpFile = processWhisperTranscribedAudio(transcribedFile)
+            speaker = getSpeaker(file)
+            allFiles[speaker] = cleanedUpFile
+    return allFiles
+
+def combineSpeakers(speakerLines: dict[str, list[TranscribedLine]]) -> list[TranscibedLineWithSpeaker]:
+    indices = {speaker: 0 for speaker in speakerLines}
+    allLines = []
+    iterableIndices = [speaker for speaker, lines in speakerLines if indices[speaker] < len(lines)]
+    mapped = map(lambda speaker: (speaker, speakerLines[speaker][indices[speaker]]), iterableIndices)
+    indexToIter = sorted(mapped, key=lambda speakerTranscribedLineTuple: speakerTranscribedLineTuple[1].timeStampStart)[0]
+    while len(iterableIndices) > 0:
+        speaker = indexToIter[0]
+        line = indexToIter[1]
+        allLines.append(TranscibedLineWithSpeaker(speaker=speaker, timeStampStart=line.timeStampStart, timeStampEnd=line.timeStampEnd, lineText=line.lineText))
+        indices[indexToIter[0]] =  indices[indexToIter[0]] + 1
+    return allLines
 
 directoryOfFiles = input('enter the directory to audio files to transcribe: ')
 directoryListDir = os.listdir(directoryOfFiles)
+preProcessedFiles = preprocessFiles(directoryOfFiles, directoryListDir)
+combineSpeakers(preProcessedFiles)
 for file in directoryListDir:
     if(file.endswith(SUPPORTED_FILE_EXTENSIONS)):
         transcribedFile = readFile(f'{directoryOfFiles}{os.sep}{file}')
-        cleanedUpFile = removeHallucination(transcribedFile)
+        cleanedUpFile = processWhisperTranscribedAudio(transcribedFile)
         with open(f'{directoryOfFiles}{os.sep}{file[:-4]}CleanedUp.txt', 'w+', encoding='utf-8') as file:
             for line in cleanedUpFile:
                 file.write(f'[{line.timeStampStart}, {line.timeStampEnd}] {line.lineText.lstrip()}\n')
